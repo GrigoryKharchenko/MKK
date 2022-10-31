212cc37b8ec7480d47829c304bbb739b1d9efef7
GrigoryKharchenko <96061286+GrigoryKharchenko@users.noreply.github.com>
2022-10-31 22:34:34 +0600

    Add dialogs for menu screen

--- app/src/main/java/com/mkk/ru/extension/FragmentExtension.kt
+++ app/src/main/java/com/mkk/ru/extension/FragmentExtension.kt
@@ -2,6 +2,7 @@ package com.mkk.ru.extension
 
 import android.content.DialogInterface
 import androidx.annotation.StringRes
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.snackbar.Snackbar
@@ -15,12 +16,31 @@ fun Fragment.showDialog(
     @StringRes message: Int,
     @StringRes positiveButton: Int,
     onClickPositiveButton: () -> Unit
-) =
+): AlertDialog =
     MaterialAlertDialogBuilder(requireContext())
         .setTitle(title)
         .setMessage(message)
         .setPositiveButton(positiveButton) { dialog: DialogInterface, _: Int ->
             onClickPositiveButton()
+            dialog.dismiss()
+        }
+        .show()
+
+fun Fragment.showDialog(
+    @StringRes title: Int,
+    @StringRes message: Int,
+    @StringRes positiveButton: Int,
+    @StringRes negativeButton: Int,
+    onClickPositiveButton: () -> Unit,
+): AlertDialog =
+    MaterialAlertDialogBuilder(requireContext())
+        .setTitle(title)
+        .setMessage(message)
+        .setPositiveButton(positiveButton) { dialog: DialogInterface, _: Int ->
+            onClickPositiveButton()
+            dialog.dismiss()
+        }
+        .setNegativeButton(negativeButton) { dialog: DialogInterface, _: Int ->
             dialog.dismiss()
         }
         .show()
--- app/src/main/java/com/mkk/ru/extension/ValidationExtension.kt
+++ app/src/main/java/com/mkk/ru/extension/ValidationExtension.kt
@@ -4,9 +4,13 @@ import java.util.regex.Pattern
 
 private const val PASSWORD_PATTERN = "^(?=\\S*[a-z])(?=\\S*[A-Z])(?=\\S*\\d)(?=\\S*[^\\w\\s])\\S{8,64}\$"
 private const val EMAIL_PATTERN = "^[a-z0-9._-]{1,63}@[a-z0-9.-]+\\.[a-z]{2,5}$"
+private const val NAME_CASHIER_PATTERN = "^[A-Za-zА-Яа-я ]+\$"
 
 fun String.isValidPassword(): Boolean =
     Pattern.compile(PASSWORD_PATTERN).matcher(this).matches()
 
 fun String.isValidEmail(): Boolean =
     Pattern.compile(EMAIL_PATTERN).matcher(this).matches()
+
+fun String.isValidFullName(): Boolean =
+    Pattern.compile(NAME_CASHIER_PATTERN).matcher(this).matches()
